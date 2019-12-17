2648d17a4b93471152e6b6f52082194f0755f8ee
jawidx <baowenbin@baidu.com>
2019-12-17 15:43:07 +0800

    Update index.ts

--- examples/index.ts
+++ examples/index.ts
@@ -59,7 +59,7 @@ const haokanConfig = {
     //     alert('右上角->在浏览器中打开');
     // },
     timeout: 2000,
-    landPage: 'http://haokan.baidu.com/download'
+    landPage: 'https://haokan.baidu.com/download'
 };
 const lanchHaokan = new LaunchApp(haokanConfig);
 
